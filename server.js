58cc3cfceb2c16d50df2407acff282caea564d5e
Muthu kumaran M <kumaran2852@gmail.com>
2026-03-02 16:34:21 +0530

    checkNginx

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 require('dotenv').config();
-app.get(process.env.API_ENDPOINT, (req, res) => {
-    console.log('Received GET request at /api/hello',req.headers);
+app.get('/checkNginx', (req, res) => {
+    console.log('Received GET request at checkNginx',req.headers);
     res.status(200).json({
         success: true,
         message: "Hello from Express server Testing the mat!"
